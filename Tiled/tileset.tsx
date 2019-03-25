20be3fa98b88fa6cdbcb0be58cba63e24d251cb8
David Navas <dnavasdiaz97@gmail.com>
2019-03-25 12:17:43 +0100

    Creado nuevo mapa con tileset

---
+++ Tiled/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset" tilewidth="16" tileheight="16" tilecount="512" columns="16">
+ <image source="../../../Descargas/Carga del Mapa/resources/tileset.png" width="256" height="512"/>
+</tileset>
