21b9b462c1964ca2458c3801eabb6029cdfb3ac9
Oscar <oscarplate@gmail.com>
2019-03-31 23:08:16 +0200

    IA v2 - Estructura principal del algoritmo

--- IA_V2/include/Astar.h
+++ IA_V2/include/Astar.h
@@ -1,17 +1,45 @@
 #ifndef ASTAR_H
 #define ASTAR_H
+#include <vector>
+#include <string>
 
+typedef struct Padre
+{
+    int x, y;
+};
+
+typedef struct Posicion
+{
+    int x, y;
+    int g, h, f;
+    std::string tag;
+    Padre padre;
+};
 
 class Astar
 {
     public:
-        Astar();
+        Astar(Posicion i, Posicion f, std::vector<Posicion> &m);
         virtual ~Astar();
+        std::vector<Posicion> mapear();
+        //TODO::-------------------------------------------------------------
+        int obtenerMenorF();
+        void removerLista(int indicePadre);
+        bool finalizarLogica(std::vector<Posicion> &listaCerr);
+        bool comprobarListaCerrada(Padre caja, std::vector<Posicion> &listaCerr);
+        bool comprobarListaAbierta(Padre caja, std::vector<Posicion> &listaAbi);
+        std::vector<Posicion> comprobarVecinos(Padre padre, std::vector<Posicion> mapa);
+        int G(Padre player, std::vector<Posicion> puntosG);
+        //-------------------------------------------------------------------
 
     protected:
 
     private:
-
+        std::vector<Posicion> *listaAbierta, *listaCerrada;
+        std::vector<Posicion> *mapaArr;
+        std::vector<Posicion> *vecinos;
+        Posicion *ini;
+        Posicion *fin;
 };
 
 #endif // ASTAR_H
--- IA_V2/src/Astar.cpp
+++ IA_V2/src/Astar.cpp
@@ -1,12 +1,60 @@
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #include "Astar.h"
 
-Astar::Astar()
+Astar::Astar(Posicion i, Posicion f, std::vector<Posicion> &m)
 {
     //ctor
+    *mapaArr = m;
+    listaCerrada->push_back(i);
+    *ini = i;
+    *fin = f;
 }
 
 Astar::~Astar()
 {
     //dtor
+    delete mapaArr;
+    delete ini;
+    delete fin;
+}
+
+std::vector<Posicion> Astar::mapear()
+{
+
+    int indiceIni = 0;
+    bool rutaEncontrada = false;
+
+    while(!rutaEncontrada)
+    {
+        vecinos = comprobarVecinos(ini, mapaArr);
+        listaAbierta->insert(listaAbierta->begin(), vecinos->begin(), vecinos->end());
+        indiceIni = obertenerMenorF();
+        *ini = listaAbierta->at(indiceIni);
+        removerLista(indiceIni);
+        rutaEncontrada = finalizarLogica(listaCerrada);
+    }
+
+    std::reverse(listaCerrada->begin(), listaCerrada->end());
+
+    Padre aux = listaCerrada->front().padre;
+    Posicion direccionIni;
+    aux.x = direccionIni.x;
+    aux.y = direccionIni.y;
+    std::vector<Posicion> salidaFinal;
+    salidaFinal.push_back(direccionIni);
+
+    for(int i = 1; i < sizeof(listaCerrada)-1; i++)
+    {
+        if(listaCerrada->at(i).x == direccionIni.x && listaCerrada->at(i).y == direccionIni.y)
+        {
+            aux = listaCerrada->at(i).padre;
+            direccionIni.x = aux.x;
+            direccionIni.y = aux.y;
+            salidaFinal.push_back(direccionIni);
+        }
+    }
+
+    return salidaFinal;
 }
