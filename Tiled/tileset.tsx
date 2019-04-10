9c7820d7380b1bdeedc33ce9fd07100017390c1c
Oscar Linares <oscarlinares293@gmail.com>
2019-04-10 18:24:51 +0200

    Revert "Merge remote-tracking branch 'origin/IA' into IA"
    This reverts commit 29bf43b02b9a5235257709ef17a84ee60b4dfe55.

--- Tiled/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset" tilewidth="16" tileheight="16" tilecount="512" columns="16">
- <image source="../Mapas/resources/tileset.png" width="256" height="512"/>
-</tileset>
